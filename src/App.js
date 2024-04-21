b0e972d4cf206f38d13c5ca1b15ff48e4baa90de
unknown <ahmedadnanshuvo@gmail.com>
2024-04-21 22:08:18 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import { useState } from 'react';
+
+
+const App = () => {
+  const [error, setError] = useState("");
+  const [value,setValue] = useState("");
+  const [chatHistory, setChatHistory] = useState([]);
+  const [isLoading,setIsLoading] = useState(false);
+
+  const surpriseOptions = [
+    'Who was the first programmer?',
+     'How be a billionaire?',
+     'Is it possible to be software engineer without coding?'
+  ];
+
+
+  const surprise = () => {
+
+    const randomValue = surpriseOptions[Math.floor(Math.random() * surpriseOptions.length)]
+    setValue(randomValue);
+  }
+
+  
+  const clear = () => {
+    setChatHistory([]);
+    setError("")
+    setValue("");
+  }
+
+
+  const getResponse = async () =>
+  {
+    if(!value){
+      setError("Error! Please ask a question");
+      return;
+    }
+
+    try {
+      const options = {
+        method : 'POST',
+        body: JSON.stringify({
+          history : chatHistory,
+          message : value
+        }),
+        headers : {
+          'Content-Type' : 'Application/json'
+        }
+      }
+      isLoading = true;
+      const response = await fetch('http://localhost:8000/gemini', options);
+      const data = await response.text();
+      isLoading = false;
+      console.log(data);
+      setChatHistory(oldChatHistory => [...oldChatHistory, {
+        role : "user",
+        parts : value
+      },
+
+      {
+        role : 'model',
+        parts : data
+      }
+    ]);
+
+    setValue("");
+
+    } catch (error) {
+      console.error(error);
+      setError("Something went wrong! Please try again later");
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+     <div className='app'>
+     <p>What do you want to know?
+     <button className='surprise' onClick={surprise} disabled={!chatHistory} >Surprise Me</button>
+     </p>
+
+     <div className='input-container'>
+      <input
+
+       value = {value}
+       placeholder='Write what you want to know'
+       onChange = {(e) => setValue(e.target.value)}
+
+      />
+      {!error && <button onClick={getResponse}>Ask Me</button>}
+      {error &&  <button onClick={clear}>Clear</button>}
+
+     </div>
+
+     {error && <p>{error}</p>}
+     {isLoading && <p>Loading...</p>}
+     
+     <div className='search-results'>
+      
+      {chatHistory.map((chatItem, _index) => <div key={_index}>
+
+        <p className='answer'>{chatItem.role} : {chatItem.parts}</p>
+      </div>)}
+
+     </div>
+
+
+     </div>
+ 
   );
 }
 
